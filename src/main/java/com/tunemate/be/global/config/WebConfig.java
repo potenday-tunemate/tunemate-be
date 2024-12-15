b5244414ed4a7c4f0082a7ebf649c5069e1b5586
milkymilky0116 <sjlee990129@gmail.com>
2024-12-15 11:39:16 +0900

    chore: update cors

--- src/main/java/com/tunemate/be/global/config/WebConfig.java
+++ src/main/java/com/tunemate/be/global/config/WebConfig.java
@@ -6,9 +6,11 @@ import com.tunemate.be.global.jwt.JwtTokenProvider;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public FilterRegistrationBean<JwtAuthenticationFilter> jwtAuthFilterRegistration(JwtTokenProvider jwtTokenProvider) {
@@ -16,5 +18,13 @@ public class WebConfig {
         registration.setFilter(new JwtAuthenticationFilter(jwtTokenProvider));
         registration.addUrlPatterns("/*");
         return registration;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 URL에 대해 적용
+                .allowedOrigins("*") // 허용할 출처
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+                .allowedHeaders("*");
     }
 }
