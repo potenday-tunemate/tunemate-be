33bdbabeb8e8a189c7ed2f7ef714fdf797b27d58
milkymilky0116 <sjlee990129@gmail.com>
2024-12-27 17:16:54 +0900

    chore: update openapi

--- src/main/java/com/tunemate/be/global/config/OpenApiConfig.java
+++ src/main/java/com/tunemate/be/global/config/OpenApiConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -20,12 +23,15 @@ public class OpenApiConfig {
                 .bearerFormat("JWT")
                 .in(SecurityScheme.In.HEADER)
                 .name("Authorization");
+        Server server = new Server();
+        server.setUrl("https://tunemate-be-production.up.railway.app.com");
 
         // Security requirement
         SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
 
         return new OpenAPI()
                 .addSecurityItem(securityRequirement)
-                .components(new Components().addSecuritySchemes("bearerAuth", bearerScheme));
+                .components(new Components().addSecuritySchemes("bearerAuth", bearerScheme))
+                .servers(List.of(server));
     }
 }
